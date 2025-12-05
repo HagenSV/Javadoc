3888431f4d8102dae09f9900945f49cbb7a4a727
HagenSV <HagenSV@users.noreply.github.com>
2025-12-05 17:30:54 +0000

    deploy: f1ac6b19d26f13f7095a69248b204264b69f6c72

---
+++ type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Farm"},{"p":"<Unnamed>","l":"Person"},{"p":"<Unnamed>","l":"Pig"}];updateSearchResults();
